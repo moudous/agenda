be7e7dea7acbe0e47a8ec13002919746ee62fcc0
moudous <marceloskype@gmail.com>
2021-04-26 10:38:51 -0300

    controllers, views

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,5 +8,50 @@ class Home extends BaseController
 	{
 		//$teste
 		return view('welcome_message');
+		$data['title'] = "Home";
+		$data['conteudo'] = view('home/index');
+		
+		$this->modelo($data);
 	}
+
+	/**
+	 * Método resonsável por listar contatos da agenda 
+	 */
+	public function contatos() : void
+	{
+		
+		$data['title'] = "Contatos";
+		$data['conteudo'] = view('home/index');
+		
+		$this->modelo($data);
+	}
+
+	/**
+	 * Contato
+	 */
+	public function contato() : void 
+	{
+
+		$data['title'] = "Contato";
+		$data['conteudo'] = view('home/index');
+		
+		$this->modelo($data);
+	}
+
+	/**
+	 * Modelo da view
+	 */
+	public function modelo() : void
+	{
+		$menu['menus'] = [
+			'Home' => base_url(),
+			'Contatos' => base_url('contatos'),
+			'Contato' => base_url('contato')
+		];
+		$data['navbar'] = view('home/padrao/navbar',$menu,['cache' => 60]);
+
+		echo view('home/padrao/modelo',$data);
+	}
+
+	
 }
